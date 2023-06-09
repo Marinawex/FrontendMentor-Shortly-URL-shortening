5edee9c90998bb61624f51808d3b55f55c0b09d7
marinawex <sheep803@gmail.com>
2023-06-09 16:13:17 -0400

    local storage

--- app/_components/Shorten.tsx
+++ app/_components/Shorten.tsx
@@ -13,22 +13,26 @@ const fetchUrl = async (url: string) => {
   return res.json();
 };
 
-
-
 function Shorten() {
   const [links, setLinks] = useState<Array<Url>>([]);
-  const [url, setUrl] = useState({ long: "", short: "", copied:false });
-  const [empty, setEmpty] = useState(false)
-
+  const [url, setUrl] = useState({ long: "", short: "", copied: false });
+  const [empty, setEmpty] = useState(false);
 
   const { isLoading, error, data, refetch } = useQuery({
-    queryKey: ["urlss", url.long],
+    queryKey: ["urls", url.long],
     queryFn: async () => {
       const data = await fetchUrl(url.long);
       const newUrl = { ...url, short: data.result.full_short_link };
       setUrl(newUrl);
-      localStorage.setItem("url", JSON.stringify(newUrl))
       setLinks([...links, newUrl]);
+      let oldData = localStorage.getItem("links") || "";
+
+      if (oldData) {
+        let oldUrls = JSON.parse(oldData);
+        localStorage.setItem("links", JSON.stringify([...oldUrls, newUrl]));
+      } else {
+        localStorage.setItem("links", JSON.stringify([newUrl]));
+      }
 
       return data;
     },
@@ -39,75 +43,60 @@ function Shorten() {
     setUrl({ ...url, long: e.target.value });
   };
 
-  const handleShortenUrl = (e : React.FormEvent) => {
+  const handleShortenUrl = (e: React.FormEvent) => {
     e.preventDefault();
-   
-   
     if (!url.long) {
-        setEmpty(!empty)
+      setEmpty(!empty);
+    } else {
+      refetch();
+      setEmpty(false);
     }
-    else{
-        refetch();
-       
-        setEmpty(false)
-    }
-  
   };
 
-  const handleCopy = (text: string, idx:number) => {
-    const copiedUrl = links.filter((lnk)=>{
-        if( lnk.short == text) {
-        return lnk  
-        }
+  const handleCopy = (text: string, idx: number) => {
+    const copiedUrl = links.filter((lnk) => {
+      if (lnk.short == text) {
+        return lnk;
+      }
+    });
 
-    })
-
-    console.log(copiedUrl);
-
-    // useEffect(() => {
-    //     // storing input name
-    //     localStorage.setItem("url", JSON.stringify(url));
-    //   }, [url]);
-
- 
-
-    
-    
-
-   
     navigator.clipboard.writeText(text);
   };
 
+  useEffect(() => {
+    let value;
+    // Get the value from local storage if it exists
+    value = localStorage.getItem("links") || "";
+    if (value) {
+      setLinks(JSON.parse(value));
+    }
+  }, []);
+
   console.log(links);
   console.log(url);
-  
- 
 
   return (
     <>
-    <form  onSubmit={handleShortenUrl}>
-      <div id="shorten">
-        {" "}
-        <div className="flex flex-col p-4 gap-3 bg-DarkViolet rounded-md m-4 bg-shorten-pattern">
-          <input
-            placeholder="Shorten a link here..."
-            className={`rounded-md p-2 ${empty ? "border-Red border-2" : ""}`}
-            value={url.long}
-            onChange={(e) => handleUrl(e)}
-          ></input>
-          <button
-            className="bg-Cyan rounded-md p-2 px-7 text-white hover:opacity-75"
-            type="submit"
-           
-          >
-            Shorten It!
-          </button>
-          { empty && <p className="text-Red text-start">Please add a link</p>}
+      <form onSubmit={handleShortenUrl}>
+        <div id="shorten">
+          {" "}
+          <div className="flex flex-col p-4 gap-3 bg-DarkViolet rounded-md m-4 bg-shorten-pattern">
+            <input
+              placeholder="Shorten a link here..."
+              className={`rounded-md p-2 ${empty ? "border-Red border-2" : ""}`}
+              value={url.long}
+              onChange={(e) => handleUrl(e)}
+            ></input>
+            <button
+              className="bg-Cyan rounded-md p-2 px-7 text-white hover:opacity-75"
+              type="submit"
+            >
+              Shorten It!
+            </button>
+            {empty && <p className="text-Red text-start">Please add a link</p>}
+          </div>
         </div>
-      </div>
       </form>
-
-
 
       {links &&
         links.map((lnk, idx) => {
@@ -117,25 +106,22 @@ function Shorten() {
               id="links"
               className="flex flex-col p-4 gap-3 rounded-md m-4  bg-grey "
             >
-              <p className="text-black text-start p-2">
-                {lnk.long}
-              </p>
+              <p className="text-black text-start p-2">{lnk.long}</p>
               <hr />
 
-              <p className="text-Cyan text-start p-2">
-                {lnk.short}
-              </p>
+              <p className="text-Cyan text-start p-2">{lnk.short}</p>
               <button
-                className={`rounded-md p-2 px-7 text-white  ${lnk.copied ? "bg-DarkViolet" : "bg-Cyan hover:opacity-75"} ` }
+                className={`rounded-md p-2 px-7 text-white  ${
+                  lnk.copied ? "bg-DarkViolet" : "bg-Cyan hover:opacity-75"
+                } `}
                 onClick={() => handleCopy(lnk.short, idx)}
               >
-                {lnk.copied ? "copied!" :'Copy'}
+                {lnk.copied ? "copied!" : "Copy"}
               </button>
               <p></p>
             </div>
           );
         })}
-        
     </>
   );
 }
